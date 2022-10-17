6ab2e1da8401acacd3749f983d054eb107096da1
Debdeepghosal <ghosaldebdeep16@gmail.com>
2022-10-18 01:39:12 +0530

    changes in request headers

--- app.js
+++ app.js
@@ -20,43 +20,36 @@ app.use(bodyparser.json());
 //video streaming route
 
 app.get("/api/v1/video/:name", async (req, res) => {
-  const range = await req.headers.range;
+  let range = await req.headers.range;
   if (!range) {
-    res.json({
-      message:
-        "requires range header try putting < range= bytes 0 > in postman",
-    });
-  } else {
-    try {
-      const videoPath = `./video/${req.params.name}.mp4`;
-      const videoSize = fs.statSync(videoPath).size;
-      console.log(videoSize);
-      const Chunk = 10 ** 6;
-      const start = Number(range.replace(/\D/g, ""));
-      const end = Math.min(start + Chunk, videoSize - 1);
-      const VideoLength = end - start + 1;
-      const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": VideoLength,
-        "Content-Type": "video/mp4",
-      };
-      res.writeHead(206, headers);
-      const readstream = fs.createReadStream(videoPath, { start, end });
-      readstream.pipe(res);
-    } catch (err) {
-      res.json({ message: "file not found" });
-    }
+    range = "bytes 0";
+  }
+  try {
+    const videoPath = `./video/${req.params.name}.mp4`;
+    const videoSize = fs.statSync(videoPath).size;
+    const Chunk = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + Chunk, videoSize - 1);
+    const VideoLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": VideoLength,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+    const readstream = fs.createReadStream(videoPath, { start, end });
+    readstream.pipe(res);
+  } catch (err) {
+    res.json({ message: "file not found" });
   }
 });
 
 //Video Upload route
 app.post("/api/v1/video/upload", (req, res) => {
   if (req.files) {
-    console.log(req.files);
     var file = req.files.file;
     var fileName = file.name;
-    console.log(fileName);
     file.mv("./video/" + fileName, function (err) {
       if (err) {
         res.json({ message: err });
